97a47412dfab0d9bd262c80a5eb0899c15fd978d
Aly Mohamed <aly.mohamed2707@gmail.com>
2026-06-01 09:53:45 +0300

    Adding api resources to make collections in postman

--- routes/api.php
+++ routes/api.php
@@ -3,11 +3,29 @@
 use App\Http\Controllers\InitController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:sanctum');
+use App\Http\Controllers\{
+    DeliveryController,
+    DeliveryStatuseController,
+    DriverController,
+    LocationController,
+    NotificationController,
+    OrderController,
+    RolesController,
+    UserController
+};
 
 Route::prefix('init')->controller(InitController::class)->group(function(){
     Route::get('models', 'models');
 });
+
+
+Route::apiResources([
+    'deliveries' => DeliveryController::class,
+    'deliveries_statuses' => DeliveryStatuseController::class,
+    'driver' => DriverController::class,
+    'locations' => LocationController::class,
+    'notifications' => NotificationController::class,
+    'orders' => OrderController::class,
+    'roles' => RolesController::class,
+    'users' => UserController::class,
+]);
